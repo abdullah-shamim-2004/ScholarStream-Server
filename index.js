06d8111b4ce13e6c08630dd6d6401df7f8264368
abdullah-shamim-2004 <abdullahshamim884@gmail.com>
2025-12-08 11:15:39 +0600

    Impliment post , get, searching method in scholarships collection.

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ async function run() {
     await client.connect();
     const db = client.db("scholar_stream_db");
     const userCollection = db.collection("users");
+    const scholarCollection = db.collection("scholarships");
 
     // User related API
     app.post("/users", async (req, res) => {
@@ -50,6 +51,49 @@ async function run() {
           message: "User created successfully",
           insertedId: result.insertedId,
         });
+      } catch (error) {
+        res.status(500).send({
+          success: false,
+          message: error.message,
+        });
+      }
+    });
+    // scholarships releted api
+    app.post("/scholarships", async (req, res) => {
+      try {
+        const scholarship = req.body;
+        const result = await scholarCollection.insertOne(scholarship);
+        res.status(201).send({
+          success: true,
+          message: "User created successfully",
+          insertedId: result.insertedId,
+        });
+      } catch (error) {
+        res.status(500).send({
+          success: false,
+          message: error.message,
+        });
+      }
+    });
+    app.get("/scholarships", async (req, res) => {
+      try {
+        const { limit, sort, search, email } = req.query;
+        let query = {};
+        if (email) {
+          query = { email: email };
+        }
+        if (search) {
+          query.scholarshipName = { $regex: search, $options: "i" };
+        }
+        let cursor = scholarCollection.find(query);
+        if (limit) {
+          cursor = cursor.limit(parseInt(limit));
+        }
+        if (sort === "top") {
+          cursor = cursor.sort({ rating: -1 });
+        }
+        const result = await cursor.toArray();
+        res.status(201).json(result);
       } catch (error) {
         res.status(500).send({
           success: false,
