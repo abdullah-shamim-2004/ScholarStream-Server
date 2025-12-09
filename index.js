6de942efd1ce4660f8318e047202171d048abd38
abdullah-shamim-2004 <abdullahshamim884@gmail.com>
2025-12-09 22:28:08 +0600

    Added Payment system with stripe.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const cors = require("cors");
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 3000;
 
@@ -26,6 +27,8 @@ async function run() {
     const db = client.db("scholar_stream_db");
     const userCollection = db.collection("users");
     const scholarCollection = db.collection("scholarships");
+    const paymentCollection = db.collection("payments");
+    const applicationCollection = db.collection("applications");
 
     // User related API
     app.post("/users", async (req, res) => {
@@ -152,6 +155,168 @@ async function run() {
         res.status(500).send({
           success: false,
           message: error.message,
+        });
+      }
+    });
+
+    // payment api
+    app.post("/create-checkout-session", async (req, res) => {
+      try {
+        const {
+          amount,
+          scholarshipName,
+          universityName,
+          scholarshipId,
+          studentEmail,
+        } = req.body;
+
+        const session = await stripe.checkout.sessions.create({
+          payment_method_types: ["card"],
+
+          line_items: [
+            {
+              price_data: {
+                currency: "usd",
+                product_data: { name: scholarshipName },
+                unit_amount: amount * 100,
+              },
+              quantity: 1,
+            },
+          ],
+
+          mode: "payment",
+          customer_email: studentEmail,
+
+          metadata: {
+            scholarshipId,
+            scholarshipName,
+            universityName,
+          },
+
+          success_url: `${process.env.VITE_CLIENT_URL}/payment-success?session_id={CHECKOUT_SESSION_ID}`,
+          cancel_url: `${process.env.VITE_CLIENT_URL}/payment-failed`,
+        });
+
+        res.send({ url: session.url });
+      } catch (error) {
+        console.log(error);
+        res.status(500).send({ message: "Stripe session error" });
+      }
+    });
+    app.get("/payment-verify", async (req, res) => {
+      try {
+        const sessionId = req.query.session_id;
+        // validate session id
+        if (!sessionId) {
+          return res
+            .status(400)
+            .json({ success: false, message: "Session ID missing" });
+        }
+
+        // Fetch Stripe sessiona)
+        const session = await stripe.checkout.sessions.retrieve(sessionId, {
+          expand: ["line_items.data.price.product"],
+        });
+        console.log("SESSION FULL:", session);
+
+        // validate session
+        if (!session) {
+          return res.status(404).json({
+            success: false,
+            message: "Invalid session",
+          });
+        }
+        // validate payment
+        if (session.payment_status !== "paid") {
+          return res.status(400).json({
+            success: false,
+            message: "Payment not completed.",
+          });
+        }
+        if (!session.metadata?.scholarshipId) {
+          return res.status(400).json({
+            success: false,
+            message: "Missing Scolarship MetaData",
+          });
+        }
+
+        // find transactionId
+        const transactionId = session.payment_intent;
+        // validate transaction id
+        if (!transactionId) {
+          return res.status(400).json({
+            success: false,
+            message: "Missing transactionId",
+          });
+        }
+        // Check duplicate payment
+        const paymentExist = await paymentCollection.findOne({ transactionId });
+        if (paymentExist) {
+          return res.status(409).json({
+            success: false,
+            message: "Payment already processed",
+            existedPayment: paymentExist,
+          });
+        }
+        // Extract metadata
+        const scholarshipId = session.metadata.scholarshipId;
+        const scholarshipName = session.metadata.scholarshipName;
+        const universityName = session.metadata.universityName;
+        // const userId = session.metadata.userId;
+        // Prepare application object
+        const applicationData = {
+          // userId,
+          userEmail: session.customer_details?.email || session.customer_email,
+          scholarshipId,
+          scholarshipName,
+          universityName,
+          transactionId,
+          amount: session.amount_total / 100,
+          currency: session.currency,
+          paymentStatus: "paid",
+          ApplicationStatus: "pending",
+          appliedAt: new Date(),
+          paidAt: new Date(),
+        };
+        // Insert into applications collection
+        const insertApplication = await applicationCollection.insertOne(
+          applicationData
+        );
+        // Insert into payment history collection
+        const paymentHistory = {
+          ...applicationData,
+          createdAt: new Date(),
+        };
+        const paymentResult = await paymentCollection.insertOne(paymentHistory);
+        return res.status(200).json({
+          success: true,
+          message: "Payment verified & application saved",
+          applicationId: insertApplication.insertedId,
+          paymentRecord: paymentResult,
+        });
+        // Retrieve paymentIntent for transaction id
+        // const paymentIntent = await stripe.paymentIntents.retrieve(
+        //   session.payment_intent
+        // );
+
+        // Prepare data to return
+        // const paymentData = {
+        //   transactionId: paymentIntent.id,
+        //   amount: session.amount_total / 100,
+        //   currency: session.currency,
+        //   scholarshipName: session.metadata.scholarshipName,
+        //   universityName: session.metadata.universityName,
+        //   userEmail: session.customer_details.email,
+        //   customerName: session.customer_details.name,
+        //   paidAt: new Date(),
+        // };
+
+        // res.status(200).json(paymentData);
+      } catch (error) {
+        console.log("Payment verify error:", error);
+        res.status(500).json({
+          message: "Failed to verify payment",
+          error: error.message,
         });
       }
     });
